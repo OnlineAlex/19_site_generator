49127e8d275a5d2a50a4f5f670dd0344570e3cf0
OnlineAlex <becasualua@gmail.com>
2018-06-21 14:57:55 +0300

    change generate logic / work with config.json / fix url problem with '&'

--- build.py
+++ build.py
@@ -1,69 +1,57 @@
-import markdown
+import markdown as md
 import os
+import json
+import collections as cl
+import html
 
 from jinja2 import Template
 
-
+RU_TITLES = json.load(open('config.json', encoding='utf-8'))
 BASIC_TEMPLATE = Template(open('templates/_basic.html').read())
 HEADER_TEMPLATE = Template(open('templates/_header.html', encoding='utf-8').read())
 FOOTER_TEMPLATE = Template(open('templates/_footer.html', encoding='utf-8').read())
 
 
+def get_topic_title(topic_slug):
+    if topic_slug.find('-') > 0:
+        topic_slug = topic_slug.replace('-', '_')
+    for topic in RU_TITLES['topics']:
+        if topic['slug'] == topic_slug:
+            return topic['title']
+
+
 def get_frendly_url(url):
     frendly_url = url[url.find('_')+1:]
-    if frendly_url.find('_') != -1:
+    if frendly_url.find('_') > 0:
         frendly_url = frendly_url.replace('_', '-')
-    return frendly_url
+    elif frendly_url.find(' ') > 0:
+        frendly_url = frendly_url.replace(' ', '-')
+    return '{}.html'.format(html.unescape(frendly_url))
 
 
-def get_title(file_name):
-    title = file_name[file_name.find('_')+1:-3]
-    if title.find('_') != -1:
-        title = title.replace('_', ' ')
-    return title.capitalize()
+def convert_md_to_html(file_source):
+    with open(os.path.join('articles', file_source), encoding='utf-8') as file_md:
+        return md.markdown(file_md.read(), extensions=['codehilite'])
 
 
-def convert_md_to_html(folder, files):
-    for file in files:
-        title = get_title(file)
-        with open(os.path.join(folder, file), encoding='utf-8') as file_md:
-            yield {
-                'title': title,
-                'code': markdown.markdown(file_md.read())
-            }
-
-
-def generate_templates_articles(articles):
+def generate_article_page(title, article):
     template = BASIC_TEMPLATE
     article_template = Template(open('templates/article.html', encoding='utf-8').read())
-    for article in articles:
-        yield {
-            'url': article['title'].replace(' ', '-').lower(),
-            'page': template.render(
-                title=article['title'],
-                header=HEADER_TEMPLATE.render(title=article['title']),
+    return template.render(
+                title=title,
+                header=HEADER_TEMPLATE.render(title=title),
                 footer=FOOTER_TEMPLATE.render(),
-                body=article_template.render(article=article['code'])
+                body=article_template.render(article=article)
             )
-        }
 
 
-def add_article_to_site(topic, articles):
-    for article in articles:
-        with open(
-            'article/{}/{}.html'.format(topic, article['url']),
-            'w',
-            encoding='utf-8'
-        ) as new_article:
-            new_article.write(article['page'])
+def add_article_to_site(article_path, article):
+    with open(os.path.join('article', article_path), 'w', encoding='utf-8') as new_article:
+        new_article.write(article)
 
 
-def add_article_info(topic, articles_name):
-    for article_name in articles_name:
-        yield {
-            'name': get_title(article_name),
-            'url': 'article/{}/{}.html'.format(topic, get_frendly_url(article_name)[:-3])
-        }
+def add_article_info(url, title):
+    return {'name': title, 'url': 'article/{}'.format(url)}
 
 
 def get_index_page(topics):
@@ -83,19 +71,29 @@ def add_index_page(page):
 
 
 if __name__ == '__main__':
-    article_folder = os.path.join(os.getcwd(), 'articles')
-    sitemap = {}
-    for root, dirs, file_names in os.walk(article_folder):
-        topic_articles = get_frendly_url(os.path.basename(root))
-        if file_names and not os.path.exists(os.path.join('article', topic_articles)):
-            os.mkdir(os.path.join('article', topic_articles))
-        if file_names:
-            html_articles = convert_md_to_html(root, file_names)
-            html_templates = generate_templates_articles(html_articles)
-            add_article_to_site(topic_articles, html_templates)
+    sitemap = cl.defaultdict(list)
+    for article_info in RU_TITLES['articles']:
+        if article_info['topic'].find('_') != -1:
+            article_info['topic'] = article_info['topic'].replace('_', '-')
 
-            topic_name = topic_articles.replace('-', ' ').capitalize()
-            sitemap[topic_name] = add_article_info(topic_articles, file_names)
+        article_url = get_frendly_url(os.path.split(article_info['source'])[-1][:-3])
+        if not os.path.exists(os.path.join('article', article_info['topic'])):
+            os.mkdir(os.path.join('article', article_info['topic']))
 
+        html_article = convert_md_to_html(article_info['source'])
+        article_page = generate_article_page(article_info['title'], html_article)
+        add_article_to_site(
+            os.path.join(article_info['topic'], article_url),
+            article_page
+        )
+        topic_title = get_topic_title(article_info['topic'])
+        sitemap[topic_title].append(add_article_info(
+            '{}/{}'.format(article_info['topic'], article_url), article_info['title']
+        ))
     index_page = get_index_page(sitemap)
     add_index_page(index_page)
+
+"""
+нормальные тайтлы на главной
+
+"""
