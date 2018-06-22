f1bbda90aba27334a19dccdd28f5ae5889d3fc62
OnlineAlex <becasualua@gmail.com>
2018-06-22 17:07:34 +0300

    del comment / rename add_article_info / add getting static_url

--- build.py
+++ build.py
@@ -1,15 +1,14 @@
 import markdown as md
-import os
+import uuid
+import os, glob
 import json
 import collections as cl
 import html
 
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+
 
 RU_TITLES = json.load(open('config.json', encoding='utf-8'))
-BASIC_TEMPLATE = Template(open('templates/_basic.html').read())
-HEADER_TEMPLATE = Template(open('templates/_header.html', encoding='utf-8').read())
-FOOTER_TEMPLATE = Template(open('templates/_footer.html', encoding='utf-8').read())
 
 
 def get_topic_title(topic_slug):
@@ -34,14 +33,14 @@ def convert_md_to_html(file_source):
         return md.markdown(file_md.read(), extensions=['codehilite'])
 
 
-def generate_article_page(title, article):
-    template = BASIC_TEMPLATE
-    article_template = Template(open('templates/article.html', encoding='utf-8').read())
+def generate_article_page(title, article, static):
+    template = Environment(
+        loader=FileSystemLoader(searchpath='templates')
+    ).get_template('article.html')
     return template.render(
                 title=title,
-                header=HEADER_TEMPLATE.render(title=title),
-                footer=FOOTER_TEMPLATE.render(),
-                body=article_template.render(article=article)
+                STATIC_URL='../../{}'.format(static),
+                article=article
             )
 
 
@@ -50,18 +49,18 @@ def add_article_to_site(article_path, article):
         new_article.write(article)
 
 
-def add_article_info(url, title):
+def get_article_info(url, title):
     return {'name': title, 'url': 'article/{}'.format(url)}
 
 
-def get_index_page(topics):
-    template = BASIC_TEMPLATE
-    article_list = Template(open('templates/index.html', encoding='utf-8').read())
+def get_index_page(topics, static):
+    template = Environment(
+        loader=FileSystemLoader(searchpath='templates')
+    ).get_template('index.html')
     return template.render(
                 title='Статьи',
-                header=HEADER_TEMPLATE.render(title='Статьи'),
-                footer=FOOTER_TEMPLATE.render(),
-                body=article_list.render(topics=topics)
+                STATIC_URL=static,
+                topics=topics
     )
 
 
@@ -71,29 +70,30 @@ def add_index_page(page):
 
 
 if __name__ == '__main__':
+    static_url = glob.glob(os.path.join(os.getcwd(), "static.*"))[0]
+    new_static_url = 'static.{}'.format(uuid.uuid4().hex[:8])
+    os.rename(static_url, new_static_url)
     sitemap = cl.defaultdict(list)
     for article_info in RU_TITLES['articles']:
-        if article_info['topic'].find('_') != -1:
-            article_info['topic'] = article_info['topic'].replace('_', '-')
-
         article_url = get_frendly_url(os.path.split(article_info['source'])[-1][:-3])
         if not os.path.exists(os.path.join('article', article_info['topic'])):
             os.mkdir(os.path.join('article', article_info['topic']))
 
         html_article = convert_md_to_html(article_info['source'])
-        article_page = generate_article_page(article_info['title'], html_article)
+        article_page = generate_article_page(
+            html.escape(article_info['title']),
+            html_article,
+            new_static_url
+        )
         add_article_to_site(
             os.path.join(article_info['topic'], article_url),
             article_page
         )
         topic_title = get_topic_title(article_info['topic'])
-        sitemap[topic_title].append(add_article_info(
-            '{}/{}'.format(article_info['topic'], article_url), article_info['title']
+        sitemap[topic_title].append(get_article_info(
+            '{}/{}'.format(article_info['topic'], article_url),
+            html.escape(article_info['title'])
         ))
-    index_page = get_index_page(sitemap)
+
+    index_page = get_index_page(sitemap, new_static_url)
     add_index_page(index_page)
-
-"""
-нормальные тайтлы на главной
-
-"""
